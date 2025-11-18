9e74a1fb4eaa159abbd7e4e7b56ea52fee22e43a
Tycjan Lamkiewicz <t.lamkiewicz@gmail.com>
2025-11-18 20:40:01 +0100

    comm

--- BetonoweZloto/Program.cs
+++ BetonoweZloto/Program.cs
@@ -2,13 +2,11 @@ using BetonoweZloto.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
--- BetonoweZloto/tailwind.config.js
+++ BetonoweZloto/tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./Components/**/*.{razor,html,cshtml}",
